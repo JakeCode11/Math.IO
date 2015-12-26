5d65274001d15dbe9a852217bdfb85234132da86
JoshuaBarnett <joshua.d.barnett@gmail.com>
2015-12-26 16:32:36 -0500

    change by josh

---
+++ Math.IO/Math.IO/Scaffolding/DependencyList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathIO
+{
+    public class DependencyList
+    {
+        public List<String> Dependencies { get; set; }
+    }
+
+}
